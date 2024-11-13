491447565dded855c1182425854eda8f5ffc9dfa
alieshenkolev <d9stingray@gmail.com>
2024-11-13 18:08:33 +0200

    feat: warehouses

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -3,13 +3,14 @@ import { Login, action as loginAction } from '../pages/Login';
 import { App } from '../App';
 import { createBrowserRouter } from 'react-router-dom';
 import { Warehouses } from '../pages/Warehouses/warehouses';
+import { Warehouse } from '../pages/Warehouse/warehouse';
 
 const routes = createRoutesFromElements(
   <Route path="/" element={<App />} errorElement={<div>IDI NAHUI</div>}>
     <Route path="login" element={<Login />} action={loginAction} />
     <Route>
-      <Route index element={<Warehouses />}></Route>
-      <Route path=":id" element={<div>Warehouse</div>} />
+      <Route index element={<Warehouses />} />
+      <Route path=":id" element={<Warehouse />} />
       <Route path=":id/products/:id" element={<div>Product</div>} />
       <Route path=":id/employees/:id" element={<div>Employee</div>} />
     </Route>
